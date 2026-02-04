ae7687168e4161dc63f5953442c40b5bf2fa00cd
killerbee90201 <168627989+killerbee90201@users.noreply.github.com>
2026-02-04 17:06:06 +0800

    Delete DLC0002/scripts directory

--- DLC0002/scripts/components/seasonmanager.lua
+++
@@ -1,8 +0,0 @@
-local Manager_RoG = require "components/seasonmanager_rog"
-local Manager_SW = require "components/seasonmanager_sw"
-
-if SaveGameIndex:IsModeShipwrecked() then
-	return Manager_SW
-else
-	return Manager_RoG
-end
